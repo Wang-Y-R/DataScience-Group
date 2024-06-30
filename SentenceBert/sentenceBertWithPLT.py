22ce989349aa471304992d4ca329c6af5b4ddd93
wyr <231250094@smail.nju.edu.cn>
2024-06-30 15:56:34 +0800

    add visualize function in sentenceBert

--- SentenceBert/sentenceBertWithPLT.py
+++ SentenceBert/sentenceBertWithPLT.py
@@ -3,6 +3,11 @@ from openpyxl.utils import get_column_letter, column_index_from_string
 from sentence_transformers import SentenceTransformer
 from sklearn.cluster import KMeans
 import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
 # 加载Sentence-Bert模型
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
@@ -10,7 +15,7 @@ src_path = '../SentenceBert/afterPreprocessed.xlsx'
 save_path = 'sentenceBertResult.xlsx'
 sentences = []
 
-finalGroupsCount = 25
+finalGroupsCount = 7 # 只需要在这里改分组数就好了
 
 def main():
     # 资源管理
@@ -35,7 +40,7 @@ def main():
     colMax = inSheet.max_column #数据源Excel最大列数
     print("There are %i rows."%(rowMax))
     for row in range(2,rowMax+1):
-        descrpition = inSheet['H%s' % row].value
+        descrpition = inSheet['D%s' % row].value
         outSheet['B%s' % row] = inSheet['A%s' % row].value
         outSheet['C%s' % row] = inSheet['B%s' % row].value
         outSheet['D%s' % row] = inSheet['C%s' % row].value
@@ -58,6 +63,20 @@ def main():
     # 打印聚类结果
     # for i, sentence in enumerate(sentences):
     #     print(f"Sentence: '{sentence}' - Cluster: {clusters[i]}")
+    
+# 使用 t-SNE 将高维句子向量映射到2维空间
+    tsne = TSNE(n_components=2, random_state=42)
+    tsne_result = tsne.fit_transform(sentence_embeddings)
+
+# 绘制 t-SNE 结果的散点图
+    plt.figure(figsize=(10, 6))
+    scatter = plt.scatter(tsne_result[:, 0], tsne_result[:, 1], c=clusters, cmap='rainbow')
+    plt.colorbar(scatter)
+    plt.title('t-SNE Visualization with K-means Clustering')
+    path = 'sentenceBertResult' + str(finalGroupsCount) + '.png'
+    plt.savefig(path,dpi = 100)
+    plt.show()
+
 
     # 讲结果存储到Excel
     for i, sentence in enumerate(sentences):
